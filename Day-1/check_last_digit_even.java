4aff2cd95d3ccbeaebdf412cd34057b823b42904
vanithavelmurugan <vani2004v@gmail.com>
2024-09-06 13:55:08 +0530

    Create check_last_digit_even.java

---
+++ Day-1/check_last_digit_even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+       // Your code here
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       if(n%2==0)
+       System.out.println(1);
+       else
+       System.out.println(0);
+    }
+}
