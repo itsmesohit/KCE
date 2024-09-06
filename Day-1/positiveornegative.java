faa334823ec52388cf31addff66adf707b65a0ea
priyakumar-2005 <priyajayasriya@gmail.com>
2024-09-06 13:54:56 +0530

    Create positiveornegative.java

---
+++ Day-1/positiveornegative.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner n = new Scanner(System.in);
+        int a =n.nextInt();
+        // Your code here
+        if(a>=0){
+            System.out.print(1);
+        }
+        else{
+            System.out.print(0);
+        }
+	
+		
+    }
+}
